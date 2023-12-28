c087bd8cb1c0d68728bfc3f945cde6a3ad28dedb
Thalisson <thalissonpop91@gmail.com>
2023-12-28 00:02:51 -0300

    Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$rAckWrOFJ3YpFgo5x/94F.WtATpENN96UHrL8BbALeGG3glbI1L3m');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$rAckWrOFJ3YpFgo5x/94F.WtATpENN96UHrL8BbALeGG3glbI1L3m');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
